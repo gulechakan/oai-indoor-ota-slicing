6617d9b4e8e22dd045f0dd81b66e9f0d9fd877af
Dustin Maas <dmaas@flux.utah.edu>
2025-04-16 15:34:07 -0600

    Remove tune cpu service. (No longer using supporting image.)

--- profile.py
+++ profile.py
@@ -161,7 +161,6 @@ def x310_node_pair(idx, x310_radio):
 
     cmd = "{} '{}' {}".format(OAI_DEPLOY_SCRIPT, oai_ran_hash, role)
     node.addService(rspec.Execute(shell="bash", command=cmd))
-    node.addService(rspec.Execute(shell="bash", command="/local/repository/bin/tune-cpu.sh"))
     node.addService(rspec.Execute(shell="bash", command="/local/repository/bin/tune-sdr-iface.sh"))
 
 def b210_nuc_pair(b210_node):
