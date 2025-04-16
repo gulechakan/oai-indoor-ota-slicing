a2a94d0aacf13b5ef04f3470efae6686a897395f
Dustin Maas <dmaas@flux.utah.edu>
2025-04-16 17:26:52 -0600

    Small edits.

--- bin/tune-sdr-iface.sh
+++ bin/tune-sdr-iface.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-SDR_IFACE=$(ifconfig | grep -B1 192.168..0.1 | grep -o "^\w*")
-
-# set all cores to performance
+# cpu tuning
 sudo apt install -y cpufrequtils
 for ((i=0;i<$(nproc);i++)); do
     sudo cpufreq-set -c $i -r -g performance
@@ -13,7 +11,8 @@ sudo sysctl -w net.core.wmem_max=62500000
 sudo sysctl -w net.core.rmem_max=62500000
 sudo sysctl -w net.core.wmem_default=62500000
 sudo sysctl -w net.core.rmem_default=62500000
-sudo ethtool -G $SDR_IFACE tx 4096 rx 4096
 
-# MTU update
+# iface tuning
+SDR_IFACE=$(ifconfig | grep -B1 192.168..0.1 | grep -o "^\w*")
 sudo ifconfig $SDR_IFACE mtu 9000
+sudo ethtool -G $SDR_IFACE tx 4096 rx 4096
